a69a1361f54d3ce13f4309bb08d9a18d8f938715
Musthafa Ali <musthu190@gmail.com>
2018-03-28 00:03:39 +0530

    Issue 3 Solved

--- login.php
+++ login.php
@@ -1,67 +1,76 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Hello World | FOSS Lab Project</title>
-        <!-- Tell the browser to be responsive to screen width -->
-        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <!-- Bootstrap 3.3.6 -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-        <!-- Ionicons -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-        <!-- Theme style -->
-        <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Hello World | FOSS Lab Project</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
 
-    </head>
-    <body class="hold-transition login-page">
-        <div class="login-box">
-            <div class="login-logo">
-                <b>Hello</b>World
-            </div>
-            <form action="validator.php" method="post">
-            <div class="login-box-body">
-                <div class="alert alert-danger alert-dismissible" id="error_box">
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                    <i class="icon fa fa-ban"></i>
-                    Invalid username or password
-                </div>
-                <div class="alert alert-warning alert-dismissible" id="warning_box">
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                    <i class="icon fa fa-warning"></i>
-                    Login to Continue
-                </div>
-                <div class="form-group has-feedback">
-                    <input type="text" class="form-control" placeholder="Enter username" name="user" required/>
-                </div>
-                <div class="form-group has-feedback">
-                    <input class="form-control" name="password" type="password" placeholder="Password" >
-                </div>
-                <div class="row">
-                    <div class="col-xs-4 pull-right">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-                    </div>
-                </div>
-            </div>
-            </form>
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="../../index2.html"><b>HELLO</b> WORLD</a>
+  </div>
+    
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    
+    <p class="login-box-msg">Sign in to start your session</p>
+
+    <form action="validator.php" method="post">
+      <div class="form-group has-feedback">
+          <input type="email" class="form-control" placeholder="Email" name="user" required="required">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+          <input type="password" class="form-control" placeholder="Password" name="password" required="required">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <a href="register.php" class="text-center">Register a new membership</a>
         </div>
-    </body>
-    <script  src="https://code.jquery.com/jquery-2.2.4.min.js"  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="  crossorigin="anonymous"></script>
-<!-- Bootstrap 3.3.6 -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.3.4/inputmask/inputmask.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.3.4/inputmask/inputmask.extensions.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.3.4/inputmask/inputmask.date.extensions.js"></script>
-    <script>
-        $(document).ready(function () {
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
+  </div>
+  <div class="alert alert-danger alert-dismissible" id="error_box">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <i class="icon fa fa-ban"></i>
+        Invalid username or password
+  </div>
+  <div class="alert alert-warning alert-dismissible" id="warning_box">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <i class="icon fa fa-warning"></i>
+        Login to Continue
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
 
-            $(":input").inputmask();
-
-        });
-    </script>
-    <script>
+<!-- jQuery 3 -->
+<script src="plugins/jQuery/jquery-2.2.3.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script>
         var status = window.location.search;
         status = status.replace("?status=", '');
         document.getElementById("error_box").disabled = true;
@@ -77,5 +86,5 @@
             $("#error_box").remove();
         }
     </script>
-
+</body>
 </html>
